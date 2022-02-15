cb9bab3e3041739f2b8afe55a2fdb09d2547ef3c
EslamBanna <solombana2000@gmail.com>
2022-02-15 19:09:39 +0200

    finish project post man

--- routes/api.php
+++ routes/api.php
@@ -34,16 +34,16 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
         Route::post('/insert-manager', [MangerContoller::class, 'insert']);
         Route::get('/get-managers', [MangerContoller::class, 'getManagers']);
         Route::get('/get-manager/{managerId}', [MangerContoller::class, 'getManager']);
-        Route::put('/update-manager/{managerId}', [MangerContoller::class, 'updateManager']);
-        Route::delete('/delete-manager/{managerId}', [MangerContoller::class, 'deleteManager']);
+        Route::post('/update-manager/{managerId}', [MangerContoller::class, 'updateManager']);
+        Route::post('/delete-manager/{managerId}', [MangerContoller::class, 'deleteManager']);
     });
-    ###################################
+    ################################################
     Route::group(['prefix' => 'malls'], function () {
         Route::post('/insert-mall', [MallController::class, 'insertMall']);
         Route::get('/get-mall/{mallId}', [MallController::class, 'getMall']);
         Route::get('/get-malls', [MallController::class, 'getMalls']);
-        Route::put('/update-mall/{mallId}', [MallController::class, 'updateMall']);
-        Route::delete('/delete-mall/{mallId}', [MallController::class, 'deleteMall']);
+        Route::post('/update-mall/{mallId}', [MallController::class, 'updateMall']);
+        Route::post('/delete-mall/{mallId}', [MallController::class, 'deleteMall']);
     });
 
     ################################################
@@ -51,36 +51,35 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
         Route::post('/insert-department', [DepartmentContoller::class, 'insertDepartment']);
         Route::get('/get-department/{departmentId}', [DepartmentContoller::class, 'getDepartment']);
         Route::get('/get-departments', [DepartmentContoller::class, 'getDepartments']);
-        Route::put('/update-department/{departmentId}', [DepartmentContoller::class, 'updateDepartment']);
-        Route::delete('/delete-department/{departmentId}', [DepartmentContoller::class, 'deleteDepartment']);
+        Route::post('/update-department/{departmentId}', [DepartmentContoller::class, 'updateDepartment']);
+        Route::post('/delete-department/{departmentId}', [DepartmentContoller::class, 'deleteDepartment']);
     });
 
-    #####################################
+    ################################################
     Route::group(['prefix' => 'vendors'], function () {
         Route::post('/insert-vendor', [VendorController::class, 'insertVendor']);
         Route::get('/get-vendor/{vendorId}', [VendorController::class, 'getVendor']);
         Route::get('/get-vendors', [VendorController::class, 'getVendors']);
-        Route::put('/update-vendor/{vendorId}', [VendorController::class, 'updateVendor']);
-        Route::delete('/delete-vendor/{vendorId}', [VendorController::class, 'deleteVendor']);
+        Route::post('/update-vendor/{vendorId}', [VendorController::class, 'updateVendor']);
+        Route::post('/delete-vendor/{vendorId}', [VendorController::class, 'deleteVendor']);
     });
-
-    ####################################
-  
 });
+################################################
 
 Route::group(['prefix' => 'products'], function () {
     Route::post('/insert-product', [ProductController::class, 'insertController']);
     Route::get('/get-product/{productId}', [ProductController::class, 'getProduct']);
     Route::get('/get-products', [ProductController::class, 'getProducts']);
-    Route::put('/update-product/{productId}', [ProductController::class, 'updateProduct']);
-    Route::delete('/delete-product/{productId}', [ProductController::class, 'deleteProduct']);
+    Route::post('/update-product/{productId}', [ProductController::class, 'updateProduct']);
+    Route::post('/delete-product/{productId}', [ProductController::class, 'deleteProduct']);
 });
 
-######################################
+################################################
 Route::group(['prefix' => 'vendor-product'], function () {
     Route::post('/create-vendor-product', [VendorProductController::class, 'createVendorProduct']);
-    Route::delete('/delete-vendor-product/{vendorProductId}', [VendorProductController::class, 'deleteVendorProduct']);
+    Route::post('/delete-vendor-product/{vendorProductId}', [VendorProductController::class, 'deleteVendorProduct']);
 });
+################################################
 
-Route::post('/register',[UserController::class,'register']);
+Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
